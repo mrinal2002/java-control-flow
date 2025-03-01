896c901e6c1ac975c7f3148a6a9d6a8189160ecf
Mrinal Patra <mrinal2002patra@gmail.com>
2025-03-01 10:21:06 +0530

    [Mrinal] desc: Added program to calculate bonus for employees based on years of service

---
+++ EmployeeBonus.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EmployeeBonus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter salary and years of service: ");
+        double salary = sc.nextDouble();
+        int years = sc.nextInt();
+
+        if (years > 5) {
+            double bonus = salary * 0.05;
+            System.out.println("Bonus amount: " + bonus);
+        } else {
+            System.out.println("No bonus awarded");
+        }
+    }
+}
